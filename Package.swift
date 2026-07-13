0efc092f6bd5b13df94daed114f8f5a64afcf3b4
Vaida <developer@vaidaf.net>
2026-07-13 11:50:16 +1000

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SoftUI",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SoftUI",
+            targets: ["SoftUI"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SoftUI",
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+        .testTarget(
+            name: "SoftUITests",
+            dependencies: ["SoftUI"],
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+    ]
+)
